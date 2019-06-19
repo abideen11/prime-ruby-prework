214ae057acd2225b8f5a6b1e367c024f5a3f9a09
Abideen Kareem <abideenkareem17@gmail.com>
2019-06-19 21:15:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime(prime_num)
+  #range_prime = (2..9)
+  #range_prime2 = 
+  limit = prime_num.ceil(Math.sqrt(prime_num))
+ for (i = 2; i <= limit; i++)
+ if (prime_num % i == 0) 
+   true 
+ else 
+   false
+end
+end 
+end
